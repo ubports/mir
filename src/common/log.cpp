1ea11eb9219d258015a9ea4de738e7248a81d547
Dalton Durst <UniversalSuperBox@users.noreply.github.com>
2018-02-23 20:29:45 -0600

    Try now

--- src/common/log.cpp
+++ src/common/log.cpp
@@ -27,7 +27,8 @@ void logv(logging::Severity sev, char const* component,
 {
     char message[1024];
     int max = sizeof(message) - 1;
-    int len = vsnprintf(message, max, fmt, va);
+    va_list my_va_copy = va_copy(va);
+    int len = vsnprintf(message, max, fmt, my_va_copy);
     if (len > max)
         len = max;
     message[len] = '\0';
